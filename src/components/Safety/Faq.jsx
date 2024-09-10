e08a08cfa247b7b657a54efcc66262ba31821e41
rkraju0810 <rajeshtamil0810@gmail.com>
2024-09-10 16:35:34 +0530

    done

--- src/components/Safety/Faq.jsx
+++ src/components/Safety/Faq.jsx
@@ -13,31 +13,57 @@ const Faq = () => {
     };
     const faqs = [
         {
-            title: "What is your policy on distribution?",
+            title: "1. Be Kind, Not Mean.",
             content:
-                "Pellentesque in nisi aliquet, pellentesque purus eget, imperdiet turpis. Fusce at enim quis neque viverra convallis. Vivamus ut elementum leo, eget tempus nisl.",
+            "We&#39;re here to lift each other up, not tear each other down. Any form of bullying, harassment, or shaming isn&#39;t cool and won&#39;t be tolerated. Treat everyone with respect, both on and off the app.",
         },
         {
-            title: "What is your policy on distribution?",
+            title: "2. Respect Privacy.",
             content:
-                "Sed viverra enim ac turpis posuere consectetur. Sed enim nibh, consequat vitae lacus eu, ullamcorper ullamcorper massa.",
+                "Keep it private. Don&#39;t share anyone&#39;s personal information—like their phone number, address, or any other details—without their consent. Respect boundaries.",
         },
         {
-            title: "What is your policy on distribution?",
+            title: "3. Spread love, not hate.",
             content:
-                "Pellentesque purus eget, imperdiet turpis. Fusce at enim quis neque viverra convallis. Vivamus ut elementum leo, eget tempus nisl.",
+                "Discrimination has no place here. We don&#39;t allow hate speech of any kind, including attacks on someone&#39;s race, gender, sexuality, religion, body type, or abilities. Everyone deserves to feel safe and welcome.",
         },
         {
-            title: "What is your policy on distribution?",
+            title: "4. Keep it real, not fake.",
             content:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac venenatis est, at convallis magna.",
+                "Honesty is the best policy. Don&#39;t spread fake news, misinformation, or conspiracy theories. If it&#39;s not true or you&#39;re not sure, don&#39;t post it. Let&#39;s keep the vibe positive and factual.",
+        },
+        {
+            title: "5. Post thoughtfully; avoid explicit content.",
+            content:
+            "We&#39;re all about self-expression, but let&#39;s keep it appropriate. Avoid posting overly explicit, sexual, or violent content. If it&#39;s graphic or could make others uncomfortable, it&#39;s best to share it elsewhere. Let&#39;s keep things light and fun here.",
+        },
+        {
+            title: "6. No spamming, please.",
+            content:
+                "Quality over quantity. Don&#39;t flood the feed with repetitive or unnecessary content. Share what matters, and keep it interesting.",
+        },
+        {
+            title: "7. Follow the law.",
+            content:
+                "If it&#39;s illegal, it&#39;s not allowed here. Don&#39;t use [App Name] to promote or engage in any illegal activities. We&#39;re here to have fun, not break the law.",
+        },
+        {
+            title: "8. 3 Strikes, and You&#39;re Out.",
+            content:
+                "We believe in second chances, but repeated rule-breaking won&#39;t fly. After three community violations, your account may be permanently suspended. Let&#39;s keep the community positive and respectful.",
+        },
+        {
+            title: "9. Report and support.",
+            content:
+                "If you see something, say something. Use our reporting tools to flag any content that violates these guidelines. We&#39;re all in this together, so let&#39;s help each other out.",
         },
     ];
+    
 
 
   return (
     <div className="lg:flex-[1_1_500px] w-full flex-none">
-        <h1 className="text-2xl my-3">Faq</h1>
+        
     {faqs.map((faq, index) => (
         <div
             key={index}
